7723dc5a4c54bb3438649794283d77c465bcdfc4
vladimiretalankin <150554739+vladimiretalankin@users.noreply.github.com>
2024-01-21 00:21:38 +0100

    Update kids-with-the-greatest-number-of-candies.rb

--- kids-with-the-greatest-number-of-candies.rb
+++ kids-with-the-greatest-number-of-candies.rb
@@ -1,1 +1,15 @@
-
+#1431
+# @param {Integer[]} candies
+# @param {Integer} extra_candies
+# @return {Boolean[]}
+def kids_with_candies(candies, extra_candies)
+    result = []
+    max = candies.max
+    candies.each_index do |i|
+      if candies[i] + extra_candies >= max
+        result[i] = true
+      else result[i] = false
+      end
+    end
+    result
+end
