6e089a22af14f5f2c0fc4efe93feff19f6929a00
vladimiretalankin <150554739+vladimiretalankin@users.noreply.github.com>
2024-10-22 00:32:38 +0200

    Update pascals-triangle.py

--- pascals-triangle.py
+++ pascals-triangle.py
@@ -1,16 +1,15 @@
 #118
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        output = [[1]]
         if numRows == 1:
-            return output
+            return [[1]]
         output = [[1],[1,1]]
         if numRows == 2:
             return output        
         for i in range(2, numRows):
             output.append([])
             output[i].append(1)
-            for j in range(1, len(output[i-1])):
+            for j in range(1, i):
                 output[i].append(output[i-1][j-1] + output[i-1][j])
             output[i].append(1)
         return output
