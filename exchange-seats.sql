ce79e3d0093a8d8c72a1031357e3130a3c5bffe6
vladimiretalankin <150554739+vladimiretalankin@users.noreply.github.com>
2024-11-04 00:16:42 +0100

    Update exchange-seats.sql

--- exchange-seats.sql
+++ exchange-seats.sql
@@ -1,9 +1,16 @@
 --626
 -- Write your PostgreSQL query statement below
-select id, 
-case 
-when id%2 > 0 and id = (select max(id) from Seat) then student
-when id % 2 > 0 then lead(student, 1) over (order by id) 
-else lag(student, 1) over (order by id)
-end as student
+
+select 
+  
+  id, 
+  
+  case 
+    when id % 2 > 0 and id = (
+      select max(id) from Seat
+    ) then student
+    when id % 2 > 0 then lead(student, 1) over (order by id) 
+    else lag(student, 1) over (order by id)
+  end as student
+  
 from Seat
