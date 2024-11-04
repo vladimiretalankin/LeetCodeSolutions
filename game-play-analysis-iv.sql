8af0c3889a2e0ca10392b0c3f430bcfc60b7bec5
vladimiretalankin <150554739+vladimiretalankin@users.noreply.github.com>
2024-11-04 19:19:20 +0100

    Update game-play-analysis-iv.sql

--- game-play-analysis-iv.sql
+++ game-play-analysis-iv.sql
@@ -1,12 +1,38 @@
 --550
 -- Write your PostgreSQL query statement below
---/*
-select ROUND(COUNT(*)::decimal / (select COUNT(distinct player_id)::decimal from Activity), 2) as fraction
+
+with players_count as (
+    select COUNT(distinct player_id)::decimal from Activity
+)
+select ROUND(COUNT(*)::decimal / (select * from players_count), 2) as fraction
 from Activity q
-inner join
-(select player_id, MIN(event_date)
- from Activity
- group by player_id) sq
- on q.player_id = sq.player_id
- and q.event_date = sq.min + 1
---*/
+inner join (
+    select player_id, MIN(event_date)
+    from Activity
+    group by player_id
+    ) sq
+on q.player_id = sq.player_id
+and q.event_date = sq.min + 1;
+
+
+-- Another way:
+
+with players_count as (
+    select COUNT(distinct player_id)::decimal from Activity
+),
+players_and_events as (
+    select 
+        event_date, 
+        rank () over w1 as event_order,
+        lead (event_date) over w1 as next_event_date
+    from Activity
+    WINDOW w1 as (
+            partition by player_id
+            order by event_date
+        )
+)
+select ROUND(COUNT(*)::decimal / (select * from players_count), 2) as fraction
+from players_and_events
+where 
+        event_order = 1
+        and next_event_date = event_date + 1
